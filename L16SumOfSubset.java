a77fb73cd2278fc2942036b8a1f65eba09ce9fa7
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-03-28 11:16:40 +0530

    initial.commit
    IT IS A JAVA RECURSION CODE FOR SUM OF SUBSET PROBLEM IN DSA

---
+++ L16SumOfSubset.java
@@ -0,0 +1,15 @@
+public class L16SumOfSubset {
+    static int countSubSet(int arr[],int n,int sum){
+        if(n==0)
+        return sum==0 ? 1 : 0;
+        return countSubSet(arr, n-1, sum) + countSubSet(arr, n-1, sum-arr[n-1]);
+    }
+    public static void main(String[] args) {
+        int n=3;
+        int arr[]={10,20,15};
+        int sum=25;
+        System.out.println(countSubSet(arr, n, sum));
+    }
+}
+
+//time complexity is Theta(2^n)
